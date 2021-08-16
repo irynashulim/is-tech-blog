74318e0c8e1944e46e15d6b759cd8d531a1fc340
Iryna <iryna.shulim@gmail.com>
2021-08-16 00:26:49 -0500

    adding style to homepage

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,32 +3,32 @@ const { Post } = require('../models');
 const postdata = [
   {
     title: 'Donec posuere metus vitae ipsum.',
-    contents: 'I love HTML',
+    contents: 'I love HTML Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book/',
     user_id: 1
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    contents: 'I love CSS',
+    contents: 'I love CSS Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ',
     user_id: 2
   },
   {
     title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    contents: 'I love JS',
+    contents: 'I love JS Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ',
     user_id: 3
   },
   {
     title: 'Nunc purus.',
-    contents: 'I love C++',
+    contents: 'I love C++ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ',
     user_id: 4
   },
   {
     title: 'Pellentesque eget nunc.',
-    contents: 'I love coding',
+    contents: 'I love coding Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ',
     user_id: 5
   },
   {
     title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    contents: 'I love React',
+    contents: 'I dont know what im doing here Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ',
     user_id: 6
   }
 ];
